17d4e733aa6d0f1fb064c6005d169829ee6a2f9a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-05-16 10:37:45 -0700

    Tabs: Cleanup

--- src/Tabs.svelte
+++ src/Tabs.svelte
@@ -5,31 +5,31 @@ export let active = tabs.length == 0 ? "" : tabs[0].name;
 
 <style>
 div {
-    max-height: 350px;
-    overflow: scroll;
+	max-height: 350px;
+	overflow: scroll;
 }
 
 .nav-item {
-    font-size: 20px;
+	font-size: 20px;
 }
 </style>
 
 <ul class="nav nav-tabs mb-2">
-    {#each tabs as tab}
-        <li class="nav-item">
-            <a class="nav-link" class:active={active == tab.name} href="#" on:click={() => active = tab.name}>{tab.name}</a>
-        </li>
-    {/each}
+	{#each tabs as tab}
+		<li class="nav-item">
+			<a class="nav-link" href="#" class:active={active == tab.name} on:click={() => active = tab.name}>{tab.name}</a>
+		</li>
+	{/each}
 </ul>
 
 <div>
-{#each tabs as tab}
-    {#if active == tab.name}
-        {#if tab.error}
-            <code>{tab.contents}</code>
-        {:else}
-            <pre>{tab.contents}</pre>
-        {/if}
-    {/if}
-{/each}
+	{#each tabs as tab}
+		{#if active == tab.name}
+			{#if tab.error}
+				<code>{tab.contents}</code>
+			{:else}
+				<pre>{tab.contents}</pre>
+			{/if}
+		{/if}
+	{/each}
 </div>
