f3d262634b7baf8512e29b6de26052acbde9a3b4
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-05-15 19:22:39 -0700

    BedViz: Spaces to tabs + update to reflect new component input + refactor to use reactive statements

--- src/BedViz.svelte
+++ src/BedViz.svelte
@@ -1,52 +1,74 @@
 <script>
-import { scaleLinear } from 'd3-scale';
-import { min, max } from 'd3-array';
+// Exports
+export let beds = [];  // Format: [{ name: "test.bed", contents: "chr1\t123\t456" }, ...]
 
-// Received properties:
-export let beds = "";
-export let names = "";
+// Imports
+import { min, max } from "d3-array";
+import { scaleLinear } from "d3-scale";
 
-// Internal globals:
-let Datasets = [];
-let XScale;
-let Width = 800;
 
+// -----------------------------------------------------------------------------
+// Globals
+// -----------------------------------------------------------------------------
+
+let names, datasets, xMin, xMax, xScale;
+let height = 400;
+let width = 800;
+
+
+// -----------------------------------------------------------------------------
 // Reactive statements:
-$: show(beds);
+// -----------------------------------------------------------------------------
 
-function parse_line(line) {
-  let l = line.split('\t');
-  return {chr: l[0], start: Number(l[1]), end: Number(l[2]), id: l[3]}
+// BED file names and intervals
+$: names = beds.map(bed => bed.name);
+$: datasets = beds.map(bed => parseBed(bed.contents));
+
+// SVG coordinates
+$: xMin = min(datasets, bed => min(bed, d => d.start));
+$: xMax = max(datasets, bed => max(bed, d => d.start))
+$: xScale = scaleLinear().domain([xMin, xMax]).range([0, width]);
+
+
+// -----------------------------------------------------------------------------
+// Utility functions
+// -----------------------------------------------------------------------------
+
+// Parse a .bed file
+function parseBed(bed) {
+	if(bed == null)
+		return [];
+	let lines = bed.split("\n");
+	return lines.filter(l => l != "").map(parseLine);
 }
-function parse_bed(bed) {
-  let lines = bed.split('\n');
-  return lines.filter(l => l != "").map(parse_line);
-}
-function show(beds) {
-  Datasets = beds.filter(bed => bed != "").map(parse_bed);
-  console.log("Datasets:", Datasets);
-  let xmin = min(Datasets, bed => min(bed, d => d.start))
-  let xmax = max(Datasets, bed => max(bed, d => d.start))
-  XScale = scaleLinear()
-    .domain([xmin, xmax])
-    .range([0, Width]);
+
+// Extract info from a .bed file line
+function parseLine(line) {
+	let l = line.split("\t");
+	return {
+		chr: l[0],
+		start: Number(l[1]),
+		end: Number(l[2]),
+		id: l[3]
+	};
 }
 </script>
 
-<div height="400" width="{Width}">
-  <svg height="400" width="{Width}">
-    {#each Datasets as row, i}
-      <g transform="translate(70, {i* 30})">
-        <text x="-70" y="20" fill="black">{names[i]}</text>
-        {#each row as interval}
-          <rect
-            x="{XScale(interval.start)}"
-            width="{XScale(interval.end) - XScale(interval.start)}"
-            y="0"
-            height="25"
-            fill="blue" />
-        {/each}
-      </g>
-    {/each}
+<div height={height} width={width}>
+  <svg height={height} width={width}>
+	{#each datasets as row, i}
+		<g transform="translate(70, {i * 30})">
+			<text x="-70" y="20" fill="black">{names[i]}</text>
+			{#each row as interval}
+			<rect
+				x={xScale(interval.start)}
+				width={xScale(interval.end) - xScale(interval.start)}
+				y="0"
+				height="25"
+				fill="blue"
+			/>
+			{/each}
+		</g>
+	{/each}
   </svg>
 </div>
