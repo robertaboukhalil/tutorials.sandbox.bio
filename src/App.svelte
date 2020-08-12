ac727b8e181b04384f6dc5170973462d504c7524
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-11 21:16:59 -0700

    Cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,10 @@
 <script>
-// TODO:
-// - Add ability to show a hint to the user
-// - Intro lesson that introduces bedtools and what it is
-// - Test in Chrome
-// - How does it look on mobile?
-
 // Bootstrap
 import popper from "popper.js";
 import Bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+// CLI Tools
 import Bedtools from "./apps/Bedtools.svelte";
 </script>
 
