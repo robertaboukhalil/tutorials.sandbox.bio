48a0c8c84d5306439ba093eeb4352a9941f8fdf6
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-12 21:45:33 -0700

    Add mock Bowtie tutorial

---
+++ src/apps/Bowtie.svelte
@@ -0,0 +1,22 @@
+<script>
+import { Lessons } from "../lessons/bowtie.js";
+import Tutorial from "../components/Tutorial.svelte";
+import VizBed from "../components/VizBed.svelte";
+</script>
+
+<Tutorial
+    let:files={files}
+	toolID="bowtie"
+	toolName="Bowtie"
+	toolAioli="bowtie/align-s/2.3.5.1"
+	toolCLI="bowtie"
+    postprocess={d => d.split("\n").filter(d => !(d.startsWith("@") || d.trim() == "")).join("\n")}
+	lessons={Lessons}>
+
+    <!-- Visualize .sam output -->
+    <div class="row mt-2" slot="viz">
+        <div class="col-12">
+            Some viz here
+        </div>
+    </div>
+</Tutorial>
